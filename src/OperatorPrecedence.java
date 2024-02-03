f403bd57a80f9fbf4134494bc5036500506edad6
Sushant Gupta <sushantg339@gmail.com>
2024-02-03 19:02:05 +0530

    i learned about operators in java and their precedence and associativity

---
+++ src/OperatorPrecedence.java
@@ -0,0 +1,6 @@
+public class OperatorPrecedence {
+    public static void main(String[] args) {
+        int a = 1+2-3*4/5;
+        System.out.println(a);
+    }
+}
---
+++ src/Operators.java
@@ -0,0 +1,16 @@
+public class Operators {
+    public static void main(String[] args) {
+        int x = 8+5 ;
+        int y = 8-5 ;
+        int z = 5*5 ;
+        float a = 7/2f ;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(a);
+        System.out.println(6>4);
+        System.out.println(6<5);
+        System.out.println(6>4 && 6>7);
+        System.out.println(6>5 || 6>9);
+    }
+}
